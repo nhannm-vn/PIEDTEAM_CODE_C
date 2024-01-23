4ec4380cdde685e5370301e349c2d2705153a7cc
nhannm-vn <nhanmin911@gmail.com>
2024-01-23 21:39:12 +0700

    23-1-2024

---
+++ testGiuaKy/test23_1_test1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+//in ra hinh chu nhat
+void printPartern(int height, int width);
+int main()
+{
+    int height = 0;
+    int width = 0;
+    printf("\nNhap vao height: ");
+    scanf("%d", &height);
+
+    printf("\nNhap vao width: ");
+    scanf("%d", &width);
+    printPartern(height, width);
+
+    return 0;
+}
+void printPartern(int height, int width){
+    for(int h = 1; h <= height; h++){
+        for(int w = 1; w <= width; w++){
+            if(h == 1 || h == height || w == 1 || w == width){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
---
+++ testGiuaKy/test23_1_test2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+//tính tích các số chẵn từ 1 đến n-1
+void mulEvenOdd(int n);
+int main()
+{
+    int n = 0;
+    char ch;
+    do{
+        printf("\nNhap vao n: ");
+        scanf("%d", &n);
+        scanf("%c", &ch);
+        fflush(stdin);
+        if(ch != 10){
+            printf("\nNhap lai di");
+            printf("\n");
+        }
+    }while(ch != 10);
+    mulEvenOdd(n);
+    return 0;
+}
+void mulEvenOdd(int n){
+    int mulEven = 1, mulOdd = 1;
+    int hasEven = 0, hasOdd = 0;
+    for(int i = 1; i <= n - 1; i++){
+        if(i % 2 == 0){
+            mulEven *= i;
+            hasEven = 1;
+        }else{
+            mulOdd *= i;
+            hasOdd = 1;
+        }
+    }
+    printf("\nTich chan: %d\nTich le: %d", mulEven*hasEven, mulOdd*hasOdd);
+}
