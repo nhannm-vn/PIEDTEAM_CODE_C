848da7ffb9a705308d40fb599ecbb3bc247128b7
nhannm-vn <nhanmin911@gmail.com>
2024-02-09 23:00:16 +0700

    9-2-2024

---
+++ testCuoiKy/test9_2_test1/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+//B2.2:
+//hàm nhận vào 1 mảng
+//removeDuplicate: xóa trùng
+//   => k còn phần tử nào trùng cả
+//   2, 1, 3, 5, 2, 4, 5, 3 => 2 1 3 5 4
+//  cách 1:
+//  => tìm phần tử trong mảng, copyArray, Nhét
+//  arr: 2, 1, 3, 5, 2, 4, 5, 3
+//  tmp:
+//  duyệt mảng arr xem arr[i]
+//      có tồn tại trong tmp k
+//      nếu arr[i] k tồn tại trong tmp
+//        thì nhét arr[i] vào
+
+//  cách 2:
+//  => tìm kiếm, count, delete
+//  arr: 2, 1, 3, 5, 2, 4, 5, 3
+
+void inputArray(int array[], int *size);
+void outputArray(int array[], int size);
+int removeDuplicate(int array[], int *size);
+int main()
+{
+    int array[100];
+    int size = 0;
+    inputArray(array, &size);
+    printf("\nCac phan tu la: ");
+    outputArray(array, size);
+
+    int tmp = removeDuplicate(array, &size);
+    if(tmp == 0){
+        printf("\nMang khong co trung");
+    }else{
+        printf("\nMang sau khi xu li: ");
+        outputArray(array, size);
+    }
+
+
+    return 0;
+}
+int removeDuplicate(int array[], int *size){
+    int flag = 0;
+    for(int i = 0; i <= *size - 2; i++){
+        for(int j = i+1; j <= *size - 1; j++){
+            if(array[j] == array[i]){
+                flag = 1;
+            }
+        }
+    }
+    if(flag == 0){
+        return 0;
+    }else{
+        int arrTmp[100];
+        int sizeTmp = 0;
+        int k = 0;
+        for(int i = 0; i <= *size - 1; i++){
+            k = 0;
+            for(int j = 0; j <= sizeTmp - 1; j++){
+                if(array[i] == arrTmp[j]){
+                    k++;
+                }
+            }
+            if(k == 0){
+                arrTmp[sizeTmp] = array[i];
+                sizeTmp++;
+            }
+        }
+        *size = 0;
+        for(int i = 0; i <= sizeTmp - 1; i++){
+            array[*size] = arrTmp[i];
+            (*size)++;
+        }
+        return 1;
+    }
+}
+void outputArray(int array[], int size){
+    for(int i = 0; i <= size - 1; i++){
+        printf("%d ", array[i]);
+    }
+}
+void inputArray(int array[], int *size){
+    printf("\nNhap vao do lon: ");
+    scanf("%d", size);
+    for(int i = 0; i <= *size - 1; i++){
+        printf("\narray[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+}
---
+++ testCuoiKy/test9_2_test2/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+//B3:
+//nhét 1 mảng khác vào 1 mảng gốc ở vị trí nào đó
+//hàm nhận vào arrMain, arrSub, pos
+//-> nhét sub vào main ở vị trí pos
+//=> ứng dụng insert| nhét
+    //arr: 2 4 6 8
+    //i  : 0 1 2 3  size:4
+
+    //sub: 1 3 5 size: 3  pos: 1
+void inputArray(int array[], int *size);
+void outputArray(int array[], int size);
+void insertArray(int array[], int *size, int pos, int key);
+int insertArrayInArray(int arrMain[], int *sizeMain, int arrSub[], int sizeSub
+                        , int pos);
+int main()
+{
+    int arrMain[100];
+    int sizeMain = 0;
+    int pos = 0;
+    int arrSub[100];
+    int sizeSub = 0;
+    inputArray(arrMain, &sizeMain);
+    printf("\nCac phan tu arrMain la: ");
+    outputArray(arrMain, sizeMain);
+
+    inputArray(arrSub, &sizeSub);
+    printf("\nCac phan tu arrSub la: ");
+    outputArray(arrSub, sizeSub);
+
+    printf("\nNhap vao pos: ");
+    scanf("%d", &pos);
+
+    int tmp = insertArrayInArray(arrMain, &sizeMain, arrSub, sizeSub, pos);
+    if(tmp == 0){
+        printf("\nVi tri khong hop li");
+    }else{
+        printf("\nMang sau khi xu li: ");
+        outputArray(arrMain, sizeMain);
+    }
+    return 0;
+}
+int insertArrayInArray(int arrMain[], int *sizeMain, int arrSub[], int sizeSub
+                        , int pos){
+    if(pos < 0 || pos > *sizeMain){
+        return 0;
+    }else{
+        for(int i = sizeSub - 1; i >= 0; i--){
+            insertArray(arrMain, sizeMain, pos, arrSub[i]);
+        }
+        return 1;
+    }
+}
+void insertArray(int array[], int *size, int pos, int key){
+    for(int i = *size - 1; i >= pos; i--){
+        array[i+1] = array[i];
+    }
+    (*size)++;
+    array[pos] = key;
+}
+void outputArray(int array[], int size){
+    for(int i = 0; i <= size - 1; i++){
+        printf("%d ", array[i]);
+    }
+}
+void inputArray(int array[], int *size){
+    printf("\nNhap vao do lon: ");
+    scanf("%d", size);
+    for(int i = 0; i <= *size - 1; i++){
+        printf("\narray[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+}
