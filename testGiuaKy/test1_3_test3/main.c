bd2d58edca0adb63395826d698441908ce80c73b
nhannm-vn <nhanmin911@gmail.com>
2024-03-01 23:35:14 +0700

    1-3-2024

--- testGiuaKy/test1_3_test3/main.c
+++ testGiuaKy/test1_3_test3/main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//*B4: xóa 1 mảng sub trong mảng arr
-//  arrInArray
-//  splice
-//  f(arr, pos, amount);
-//   trong mảng arr xóa từ pos số lượng amount pt
-//  //hậu bối của bài delete
-//  arr: 2 4 6 8 10   pos 1  amount: 2
-//    i  0 1 2 3 4
+//splice
 void inputArray(int array[], int *size);
 void outputArray(int array[], int size);
 int splice(int array[], int *size, int pos, int amount);
@@ -18,9 +11,8 @@ int main()
     int pos = 0;
     int amount = 0;
     inputArray(array, &size);
-    printf("\nCac phan tu la: ");
+    printf("\nMang ne: ");
     outputArray(array, size);
-
     printf("\nNhap vao pos: ");
     scanf("%d", &pos);
     printf("\nNhap vao amount: ");
@@ -30,7 +22,7 @@ int main()
     if(result == 0){
         printf("\nVi tri khong hop li");
     }else{
-        printf("\nMang sau khi xu li: ");
+        printf("\nMang sau khi xoa: ");
         outputArray(array, size);
     }
     return 0;
@@ -39,13 +31,15 @@ int splice(int array[], int *size, int pos, int amount){
     if(pos < 0 || pos >= *size){
         return 0;
     }
-    for(int i = pos; i <= *size - 1; i++){
+    for(int i = pos; i <= *size - 1 - amount; i++){
         array[i] = array[i + amount];
     }
     amount = *size - pos >= amount ? amount : *size - pos;
     (*size) -= amount;
+
     return 1;
 }
+
 void inputArray(int array[], int *size){
     printf("\nNhap vao do lon: ");
     scanf("%d", size);
