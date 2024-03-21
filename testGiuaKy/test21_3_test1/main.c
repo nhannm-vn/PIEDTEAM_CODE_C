8a0d269c87c6c4de6e170a86ca5c5d2bb749357a
nhannm-vn <nhanmin911@gmail.com>
2024-03-21 09:43:03 +0700

    21-3-2024

---
+++ testGiuaKy/test21_3_test1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+void inputArray(int array[], int *size);
+void outputArray(int array[], int size);
+int main()
+{
+    int array[100];
+    int size = 0;
+    inputArray(array, &size);
+    printf("\nMang ne: ");
+    outputArray(array, size);
+
+    return 0;
+}
+void inputArray(int array[], int *size){
+    printf("\nNhap vao do lon: ");
+    scanf("%d", size);
+    for(int i = 0; i <= *size - 1; i++){
+        printf("\narray[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+}
+void outputArray(int array[], int size){
+    for(int i = 0; i <= size - 1; i++){
+        printf("%d ", array[i]);
+    }
+}
